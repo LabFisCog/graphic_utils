888b845b01430544e40bf0b761dc09fbec96dce3
Felipe Picard <44078452+FelipePicard@users.noreply.github.com>
2025-06-23 19:11:39 -0300

    Update slicer.py

--- slicer.py
+++ slicer.py
@@ -5,9 +5,8 @@
 import numpy as np
 import cv2
 import sys
-
-up_dataset = [(695, 442), (614, 462), (563, 482), (518, 502), (476, 522), (434, 542), (392, 562), (352, 582), (311, 603), (269, 625), (225, 649), (179, 675), (130, 704), (77, 737), (14, 780), (55, 760), (112, 723), (163, 692), (210, 665), (254, 641), (299, 617), (342, 595), (387, 572), (429, 552), (471, 532), (513, 512), (557, 492), (607, 472), (673, 452), (763, 466), (816, 535), (838, 579), (861, 622), (896, 677), (1016, 707), (1125, 687), (1236, 667), (1362, 704), (1434, 772), (1503, 754), (1547, 732), (1592, 707), (1643, 676), (1694, 645), (1742, 617), (1783, 596), (1829, 573), (1876, 553), (1948, 533), (2155, 517), (2392, 537), (2443, 539), (2260, 519), (1971, 521), (1890, 541), (1838, 561), (1797, 581), (1754, 603), (1709, 628), (1659, 658), (1607, 690), (1559, 718), (1514, 741), (1466, 761), (1392, 731), (1311, 658), (1155, 674), (1060, 694), (925, 695), (871, 627), (846, 581), (822, 533), (772, 465)]
-down_dataset = [(672, 1046), (588, 1066), (531, 1086), (483, 1106), (437, 1126), (392, 1147), (348, 1170), (304, 1194), (255, 1223), (202, 1256), (140, 1298), (65, 1353), (46, 1377), (128, 1315), (193, 1270), (247, 1236), (297, 1206), (342, 1181), (386, 1158), (429, 1137), (475, 1117), (521, 1097), (576, 1077), (648, 1057), (745, 1084), (765, 1152), (785, 1247), (808, 1295), (905, 1334), (969, 1314), (1016, 1294), (1059, 1274), (1113, 1254), (1187, 1277), (1239, 1335), (1332, 1331), (1394, 1311), (1453, 1291), (1561, 1279), (1617, 1309), (1679, 1350), (1789, 1352), (1844, 1332), (1901, 1312), (1953, 1292), (1999, 1272), (2046, 1252), (2104, 1232), (2186, 1212), (2382, 1196), (2455, 1216), (2458, 1211), (2384, 1191), (2186, 1205), (2103, 1225), (2045, 1245), (1998, 1265), (1952, 1285), (1900, 1305), (1843, 1325), (1788, 1345), (1687, 1347), (1630, 1312), (1571, 1276), (1459, 1282), (1399, 1302), (1338, 1322), (1247, 1332), (1192, 1272), (1108, 1248), (1056, 1268), (1012, 1288), (966, 1308), (898, 1328), (811, 1287), (789, 1235), (769, 1135), (745, 1071)]
+from scipy.interpolate import interp1d, CubicSpline
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 3:
         sys.exit("Usage: python slicer.py path/to/image.png number_of_sections")
@@ -15,9 +14,11 @@ if len(sys.argv) != 3:
 
 # save image file-name or path to variable f
 f = sys.argv[1]
-n_sections = sys.argv[2]
+n_sections = int(sys.argv[2])
 
-interpoint_distance = 2
+interpoint_distance = 40
+dist_threshold = 10
+resolution = 600
 
 def get_contours(img):
     # gets the image and extracts the points from the outter and inner contours
@@ -49,27 +50,19 @@ def get_contours(img):
             down.append((x, y))
             cv2.circle(image, (x, y), 10, (255, 0, 255), -1)
     
-    # # OBS.: talvez seja melhor botar um espaçamento maior entre cada pontinho
-    # for contour in up_contour:
-    #     for point in contour:
-    #         x, y = point[0]
-    #         up.append((x,y))
-    #         cv2.circle(image, (x, y), 20, (0, 255, 255), -1)
-
-
-    # for contour in down_contour:
-    #      for point in contour:
-    #         x, y = point[0]
-    #         down.append((x,y))
-    #         cv2.circle(image, (x, y), 30, (255, 0, 255), -1)
     if image is None:
          print('Image could not be opened')
    
     # image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
-    cv2.imshow('exemplo', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-   
+    # cv2.imshow('exemplo', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
+    up = np.array(up)
+    down = np.array(down)
+
+    up = up[np.argsort(up[:, 0])]
+    down = down[np.argsort(down[:, 0])]
     # print("UP!!! ", up)
     # print("\n\n\n")
     # print("DOWN!!!", down)
@@ -79,22 +72,30 @@ def get_contours(img):
 
 
    
-def interpolate_contours(contour, n_sections):
+def interpolate_contours(contour, resolution, n_sections):
     # receives a contour and divides its length by the desired number of sections
     # first, interpolate the curve
-
+    points = np.array(contour)
 
     # second, get the length of the curve
+    # Parametriza por comprimento de arco
+    dists = np.sqrt(np.sum(np.diff(points, axis=0)**2, axis=1))
+    t = np.concatenate(([0], np.cumsum(dists)))  # comprimento acumulado
+    if len(t) != len(points):
+        raise ValueError("Número de parâmetros t não bate com número de pontos")
 
+    # Interpola x(t) e y(t)
+    cs_x = CubicSpline(t, points[:, 0])
+    cs_y = CubicSpline(t, points[:, 1])
 
     # third, divide the length of the curve by the number of sections
+    t_new = np.linspace(0, t[-1], n_sections)
+    x_new = cs_x(t_new)
+    y_new = cs_y(t_new)
 
 
-    # fourth, create a list with the points that will be evenly spaced along this curve
-
-
-    # retornar uma lista com os pontos igualmente espaçados no contorno externo e interno
-    return # spaced_points
+    # fourth, create and return a list with the points that will be evenly spaced along this curve
+    return np.stack((x_new, y_new), axis=1)
 
 
 # OPCIONAL (FAZER POR ULTIMO)
@@ -102,8 +103,8 @@ def compute_centerline(outer_contour, inner_contour):
     centerpoints = []
     # gets the outer and inner contour and computes the centerline between them
     for outer_point, inner_point in zip(outer_contour, inner_contour):
-        print('outer', outer_point)
-        print('inner', inner_point)
+        # print('outer', outer_point)
+        # print('inner', inner_point)
 
         
 
@@ -112,13 +113,13 @@ def compute_centerline(outer_contour, inner_contour):
         # centerpoints = np.linalg.norm(outer_array - inner_array)/2
 
         centerpoint = (center_x, center_y)
-        print("centerpoint ", centerpoint)
+        # print("centerpoint ", centerpoint)
         centerpoints.append(centerpoint)
-        print("centerpoints lista:  ", centerpoints)
+        # print("centerpoints lista:  ", centerpoints)
         
     return centerpoints
 
-compute_centerline(up_dataset, down_dataset)
+
 
 
 def divide_sections(centerline, outer_contour, inner_contour, n_sections):
@@ -135,4 +136,37 @@ def visualize(img, outer_points, inner_points):
     return
 
 
-get_contours(f)
+up_c, dw_c = get_contours(f)
+
+print("TAMANHO UPPER", len(up_c))
+print("TAMANHO LOWER", len(dw_c))
+
+centerline = compute_centerline(up_c, dw_c)
+
+spaced_up = interpolate_contours(up_c, resolution, n_sections)
+spaced_down = interpolate_contours(dw_c, resolution, n_sections)
+spaced_cl = interpolate_contours(centerline, resolution, n_sections)
+
+# Visualização
+plt.figure(figsize=(8, 6))
+original_points_np_up = np.array(up_c)
+original_points_np_down = np.array(dw_c)
+xs = (0, 100, 1)
+plt.plot(original_points_np_up[:, 0], original_points_np_up[:, 1], 'ro-', markersize=3, label='Originais_upper')
+plt.plot(original_points_np_down[:, 0], original_points_np_down[:, 1], 'go-', markersize=3, label='Originais_lower')
+
+# Plota as linhas que conectam os pares
+for p_up, p_down in zip(spaced_up, spaced_down):
+    x_vals = [p_up[0], p_down[0]]
+    y_vals = [p_up[1], p_down[1]]
+    plt.plot(x_vals, y_vals, color='blue')
+
+plt.plot(spaced_up[:, 0], spaced_up[:, 1], 'bo', markersize=8, label='Interpolados_upper')
+plt.plot(spaced_down[:, 0], spaced_down[:, 1], 'bo', markersize=8, label='Interpolados_lower')
+plt.plot(spaced_cl[:, 0], spaced_cl[:, 1], 'ro-', markersize=2, label='Interpolados_cl')
+
+plt.legend()
+plt.title("Interpolação de Contorno")
+plt.axis('equal')
+plt.grid(True)
+plt.show()
