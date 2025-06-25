add413b4f16d41429a0e29880de94d2f0664f42d
Felipe Picard <44078452+FelipePicard@users.noreply.github.com>
2025-06-25 00:51:13 -0300

    Add files via upload

--- linear_ri.py
+++ linear_ri.py
@@ -2,15 +2,18 @@ import numpy as np
 import pandas as pd
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib import pyplot as plt
+import colorizer3 as c
 
-df = pd.read_excel('/Users/felipepicard/Documents/01- Fisiologia da Cognição/Experimentos/01 - Mapeamento Motor Callithrix/analise_sitios.xlsx', 'conexões')
+# open dataframe
+df = pd.read_excel('/Users/felipepicard/Documents/01- Fisiologia da Cognição/Experimentos/01 - Mapeamento Motor Callithrix/analise_sitios.xlsx', 'dist_cortical')
 
 # figure scale (set as 1 for arbitrary values)
 scale = 1
 
-ri_dict = {}
+ri_array = []
+get_mean_of_representations = True
 
-def calculate_weight(ps, ss):
+def calculate_weight(ps, ss, ipsi=False):
     # getting the x and y coordinates for the primary and secondary sites
     ps_x = df.iloc[ps, 1]
     ps_y = df.iloc[ps, 2]
@@ -25,51 +28,50 @@ def calculate_weight(ps, ss):
 
     return int(dist)
 
-def linear_ri(df):
-    for site in range(len(df)):
-        
-
-
-
-
-
-
-
-
-
 # go through the columns in the dataframe (df)
 for site in range(len(df)):
     weight = []
     buffer = []
-    # go through the secondary responses for the primary site (sr_ps)
+    ps_index = site
+    # go through the responses for the primary site (r_ps)
     for response in range(4,8):
-        ps_index = site
-        sr_ps = df.iloc[site, response]
-        #print("Secondary response in primary site: ", sr_ps)
-        # for each secondary response, check which other sites have this response as their primary response
+        # initialize ipsi as false since most sites are only contralateral
+        ipsi = False
+        resp_ps = df.iloc[site, response]
+        # check if it is ipsilateral
+        if isinstance(resp_ps, str) and resp_ps.startswith('i'):
+            resp_ps = resp_ps[1:]
+            ipsi = True
+            # print("Secondary response in primary site: ", sr_ps)
+            # for each secondary response, check which other sites have this response as their primary response
         for secondary_site in range(len(df)):
-            # if the primary response of the secondary site (pr_ss) is the same as the secondary response in the primary site, calculate the index
-            ss_index = secondary_site
-            pr_ss = df.iloc[secondary_site, 3]
-            if (pr_ss == sr_ps):
-                #print("PR_SEC_SITE", site+1, pr_ss)
-                #print("SR_PRI_SITE", site+1, sr_ps)
-                #print("Primary response in secondary site: ", pr_ss)
-                w = calculate_weight(ps_index, ss_index)
-                buffer.append(w)
-                #print("BUFFER", site+1, buffer)
-        #print("NEXT LIMB!!!!")
-        # sum all the values in buffer and divide by its length to get the mean weight
-        mean_weight = 0
-        if (len(buffer) != 0):
-            mean_weight = sum(buffer)/len(buffer)
-        # add the mean_weight to the weight array
-        #print("MEAN WEIGHT", site+1, mean_weight)
-        weight.append(mean_weight)
-        # free/clear the buffer
+            for response in range (9, 14):
+                ss_index = secondary_site
+                resp_ss = df.iloc[secondary_site, response]
+                if (resp_ps == resp_ss):
+                    w = calculate_weight(ps_index, ss_index)
+                    buffer.append(w)
+
+        new_weight = 0
+        if get_mean_of_representations == True:
+            if (len(buffer) != 0):
+                new_weight = sum(buffer)/len(buffer)
+        else:
+            new_weight = sum(buffer)
+        weight.append(new_weight)
         buffer.clear()
 
-    #print("WEIGHT", site+1, weight)
-    # sum all the weights in the weight list to calculate the recruitment index (ri)
     ri = sum(weight)
-    print("Recruitment Index", site+1, ": ", ri)
+    ri_array.append(ri)
+    ri_form = str(ri)[:6].replace(".",",")
+    # print("Recruitment Index", site+1, ": ", ri)
+    print("Recruitment Index", site+1, ": ", ri_form)
+    
+
+#print("WEIGHT", site+1, weight)
+
+ri_array = np.array(ri_array)
+ri_array = ri_array/(np.max(ri_array))
+print(ri_array)
+
+c.colorize(ri_array, "ri_penfield")
