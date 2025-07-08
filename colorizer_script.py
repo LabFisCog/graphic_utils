b8914721f4cea603a5fdf95d0ec5f589f44aee47
Felipe Picard <felipe.picard@hotmail.com>
2025-07-08 00:08:57 -0300

    adding newer versions of the files

--- colorizer_script.py
+++ colorizer_script.py
@@ -3,10 +3,9 @@ import numpy as np
 import pandas as pd
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib import pyplot as plt
-import teste
 
 if len(sys.argv) != 3:
-        sys.exit("Usage: python voronoi.py path/to/sheet.xlsx file_name")
+        sys.exit("Usage: python colorizer_script.py path/to/sheet.xlsx output_file_name")
 
 ct = pd.read_excel(sys.argv[1])
 
@@ -18,6 +17,91 @@ data = {
     "mode": [],
     "position": []
 }
+
+# voronoi util to allow us to tweak properties in its regions using matplotlib
+def voronoi_finite_polygons_2d(vor, radius=None):
+    """
+    Reconstruct infinite voronoi regions in a 2D diagram to finite
+    regions.
+
+    Parameters
+    ----------
+    vor : Voronoi
+        Input diagram
+    radius : float, optional
+        Distance to 'points at infinity'.
+
+    Returns
+    -------
+    regions : list of tuples
+        Indices of vertices in each revised Voronoi regions.
+    vertices : list of tuples
+        Coordinates for revised Voronoi vertices. Same as coordinates
+        of input vertices, with 'points at infinity' appended to the
+        end.
+
+    """
+
+    if vor.points.shape[1] != 2:
+        raise ValueError("Requires 2D input")
+
+    new_regions = []
+    new_vertices = vor.vertices.tolist()
+
+    center = vor.points.mean(axis=0)
+    if radius is None:
+        radius = np.ptp(vor.points, axis=0).max()
+
+    # Construct a map containing all ridges for a given point
+    all_ridges = {}
+    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
+        all_ridges.setdefault(p1, []).append((p2, v1, v2))
+        all_ridges.setdefault(p2, []).append((p1, v1, v2))
+
+    # Reconstruct infinite regions
+    for p1, region in enumerate(vor.point_region):
+        vertices = vor.regions[region]
+
+        if all(v >= 0 for v in vertices):
+            # finite region
+            new_regions.append(vertices)
+            continue
+
+        # reconstruct a non-finite region
+        ridges = all_ridges[p1]
+        new_region = [v for v in vertices if v >= 0]
+
+        for p2, v1, v2 in ridges:
+            if v2 < 0:
+                v1, v2 = v2, v1
+            if v1 >= 0:
+                # finite ridge: already in the region
+                continue
+
+            # Compute the missing endpoint of an infinite ridge
+
+            t = vor.points[p2] - vor.points[p1] # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
+
+            midpoint = vor.points[[p1, p2]].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[v2] + direction * radius
+
+            new_region.append(len(new_vertices))
+            new_vertices.append(far_point.tolist())
+
+        # sort region counterclockwise
+        vs = np.asarray([new_vertices[v] for v in new_region])
+        c = vs.mean(axis=0)
+        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+        new_region = np.array(new_region)[np.argsort(angles)]
+
+        # finish
+        new_regions.append(new_region.tolist())
+
+    return new_regions, np.asarray(new_vertices)
+
 
 def populate_data(ct):
     # populate the data dictionary
@@ -54,7 +138,7 @@ def grayscale(index):
     gray = 0
     
     if pd.isna(data["ri"][index]):
-        color = [1, 0, 1]
+        color = [1, 1, 0.6235]
         hatch = '////'
 
     else:
@@ -106,7 +190,7 @@ def colorize(data, fname, v_max):
     vor = Voronoi(points)
 
     # generate regions
-    regions, vertices = teste.voronoi_finite_polygons_2d(vor)
+    regions, vertices = voronoi_finite_polygons_2d(vor)
 
     fig, ax = plt.subplots(figsize=(scale*1.274, scale))  # Adjust the width and height as needed
 
--- linear_ri.py
+++ linear_ri.py
@@ -1,56 +1,74 @@
 import numpy as np
 import pandas as pd
+from math import sqrt
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib import pyplot as plt
-import colorizer3 as c
+# import colorizer3 as c
 
 # open dataframe
-df = pd.read_excel('/Users/felipepicard/Documents/01- Fisiologia da Cognição/Experimentos/01 - Mapeamento Motor Callithrix/analise_sitios.xlsx', 'dist_cortical')
+df_general = pd.read_excel('dados.xlsx', 'Sheet1_geral')
+df_cs01 = pd.read_excel('dados.xlsx', 'Sheet2_ld')
 
 # figure scale (set as 1 for arbitrary values)
 scale = 1
 
 ri_array = []
-get_mean_of_representations = True
+get_mean_of_representations = False
+normalize_irm = False
+
+cc_x = float(df_general.iloc[0, 9])
+cc_y = float(df_general.iloc[1, 9])
+print("CC_x = ", cc_x)
+print("CC_y = ", cc_y)
 
 def calculate_weight(ps, ss, ipsi=False):
     # getting the x and y coordinates for the primary and secondary sites
-    ps_x = df.iloc[ps, 1]
-    ps_y = df.iloc[ps, 2]
-    ss_x = df.iloc[ss, 1]
-    ss_y = df.iloc[ss, 2]
+    ps_x = float(df_cs01.iloc[ps, 0])
+    ss_x = float(df_general.iloc[ss, 0])
 
-    ps_pos = np.array((ps_x, ps_y))
-    ss_pos = np.array((ss_x, ss_y))
+    psite = np.array((ps_x, 0))
+    ssite = np.array((ss_x, 0))
+    cc = np.array((cc_x, cc_y))
 
-    # calculating the Euclidean distance and multiplying it by the scale factor
-    dist = np.linalg.norm (ps_pos - ss_pos) * scale
+    dist_ipsi = 0
 
-    return int(dist)
+    # print(f"Primary site {ps+1} position: ", ps_x)
+    # print(f"Secondary site {ss+1} position: ", ss_x)
+    if ipsi == True:
+        dist = (np.linalg.norm(psite - cc) + np.linalg.norm(cc - ssite)) * scale
+        print("dist ipsi = ", dist_ipsi)
+
+    else:
+        # calculating the Euclidean distance and multiplying it by the scale factor
+        dist = dist_ipsi + (sqrt((ps_x - ss_x)**2) * scale)
+
+    return float(dist)
 
 # go through the columns in the dataframe (df)
-for site in range(len(df)):
+for site in range(len(df_cs01)):
     weight = []
     buffer = []
     ps_index = site
     # go through the responses for the primary site (r_ps)
-    for response in range(4,8):
+    for response in range(1,5):
         # initialize ipsi as false since most sites are only contralateral
         ipsi = False
-        resp_ps = df.iloc[site, response]
+        resp_ps = df_cs01.iloc[site, response]
         # check if it is ipsilateral
         if isinstance(resp_ps, str) and resp_ps.startswith('i'):
+            print("ipsi response: ", resp_ps)
             resp_ps = resp_ps[1:]
             ipsi = True
             # print("Secondary response in primary site: ", sr_ps)
             # for each secondary response, check which other sites have this response as their primary response
-        for secondary_site in range(len(df)):
-            for response in range (9, 14):
+        for secondary_site in range(len(df_general)):
+            for response in range (1, 6):
                 ss_index = secondary_site
-                resp_ss = df.iloc[secondary_site, response]
-                if (resp_ps == resp_ss):
-                    w = calculate_weight(ps_index, ss_index)
+                resp_ss = df_general.iloc[secondary_site, response]
+                if isinstance(resp_ps, str) and resp_ps == resp_ss:
+                    w = calculate_weight(ps_index, ss_index, ipsi)
                     buffer.append(w)
+                    ipsi = False
 
         new_weight = 0
         if get_mean_of_representations == True:
@@ -68,10 +86,13 @@ for site in range(len(df)):
     print("Recruitment Index", site+1, ": ", ri_form)
     
 
-#print("WEIGHT", site+1, weight)
+# print("WEIGHT", site+1, weight)
 
 ri_array = np.array(ri_array)
-ri_array = ri_array/(np.max(ri_array))
-print(ri_array)
 
-c.colorize(ri_array, "ri_penfield")
+if normalize_irm == True:
+    print("Max val = ", np.max(ri_array))
+    ri_array = ri_array/(np.max(ri_array))
+print("Recruitment index array: ", ri_array)
+
+# c.colorize(ri_array, "ri_penfield")
